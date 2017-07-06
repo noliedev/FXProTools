0c01cd5ac9f79671773662ee0fc29e088f06a193
John Benedict <jben0324@gmail.com>
2017-07-06 23:13:10 +0800

    Partial Admin functions

--- wp-content/themes/storefront-child/inc/class-storefront-admin.php
+++ wp-content/themes/storefront-child/inc/class-storefront-admin.php
@@ -20,7 +20,11 @@ if(!class_exists('StoreFrontAdmin')){
 			add_action('admin_enqueue_scripts', array($this, 'enqueue_admin_styles'));
 			add_action('login_enqueue_scripts',  array($this, 'enqueue_admin_styles'));
 			add_filter('login_headerurl', array($this, 'login_logo_link'));
-			add_action('login_enqueue_scripts',  array($this, 'remove_admin_menus'));
+			add_action('admin_menu',  array($this, 'remove_admin_menus'), 99);
+			add_action('admin_init', array($this, 'remove_dashboard_meta'));
+			add_filter('login_redirect', array($this, 'dashboard_redirect'));
+
+			add_action('admin_menu', array($this, 'test_page'));
 		}
 
 		// Storefront Styles - Admin
@@ -44,17 +48,75 @@ if(!class_exists('StoreFrontAdmin')){
 		// Remove Admin Menus
 		public function remove_admin_menus()
 		{
-			// remove_menu_page('index.php');                  // Dashboard
-			// remove_menu_page('jetpack');                    // Jetpack
-			// remove_menu_page('edit.php');                   // Posts
-			// remove_menu_page('upload.php');                 // Media
-			// remove_menu_page('edit.php?post_type=page');    // Pages
-			// remove_menu_page('edit-comments.php');          // Comments
-			// remove_menu_page('themes.php');                 // Appearance
-			// remove_menu_page('plugins.php');                // Plugins
-			// remove_menu_page('users.php');                  // Users
-			// remove_menu_page('tools.php');                  // Tools
-			// remove_menu_page('options-general.php');        // Settings
+			remove_menu_page('index.php');                  // Dashboard
+			remove_menu_page('jetpack');                    // Jetpack
+			remove_menu_page('edit.php');                   // Posts
+			remove_menu_page('upload.php');                 // Media
+			remove_menu_page('edit.php?post_type=page');    // Pages
+			remove_menu_page('edit-comments.php');          // Comments
+			remove_menu_page('themes.php');                 // Appearance
+			remove_menu_page('plugins.php');                // Plugins
+			remove_menu_page('users.php');                  // Users
+			remove_menu_page('tools.php');                  // Tools
+			remove_menu_page('options-general.php');        // Settings
+		}
+
+		// Remove Dashboard Widgets
+		public function remove_dashboard_meta()
+		{
+			if (!current_user_can( 'manage_options')){
+				remove_meta_box('dashboard_incoming_links', 'dashboard', 'normal');
+				remove_meta_box('dashboard_plugins', 'dashboard', 'normal');
+				remove_meta_box('dashboard_primary', 'dashboard', 'normal');
+				remove_meta_box('dashboard_secondary', 'dashboard', 'normal');
+				remove_meta_box('dashboard_quick_press', 'dashboard', 'side');
+				remove_meta_box('dashboard_recent_drafts', 'dashboard', 'side');
+				remove_meta_box('dashboard_recent_comments', 'dashboard', 'normal');
+				remove_meta_box('dashboard_right_now', 'dashboard', 'normal');
+				remove_meta_box('dashboard_activity', 'dashboard', 'normal');
+			}
+		}
+
+		// Make Affiliate Overview as dashboard
+		public function dashboard_redirect($url)
+		{
+			$redirect = 'wp-admin/admin.php?page=affiliate-wp';
+			$roles = array('administrator');
+			if(isset($user->roles) && is_array($user->roles)){
+				if (in_array($roles, $user->roles)){
+					return $redirect;
+				} else {
+					return home_url();
+				}
+			} else {
+				return $redirect;
+			}
+		}
+
+		// Custom page - Test
+		public function test_page()
+		{
+			$page_settings = add_menu_page(
+				'Test Page',
+				'Test Page',
+				'manage_options',
+				'test-page',
+				'page_content',
+				'dashicons-email',
+				9
+			);
+			add_action('load-' . $page_settings, 'page_assets');
+
+			function page_assets(){	
+				// CSS/JS					
+			}
+
+			function page_content(){
+				$screen = get_current_screen();
+				echo '<pre>';
+				print_r($screen);
+				echo '</pre>';
+			}
 		}
 
 	}
