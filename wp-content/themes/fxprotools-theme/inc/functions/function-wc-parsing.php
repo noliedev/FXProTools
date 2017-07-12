f810c66f917ab62c31914026e8810ca81e03e747
John Benedict <jben0324@gmail.com>
2017-07-12 22:56:10 +0800

    Updated add subscription function - set status as active, add start date and next payment date

--- wp-content/themes/fxprotools-theme/inc/functions/function-wc-parsing.php
+++ wp-content/themes/fxprotools-theme/inc/functions/function-wc-parsing.php
@@ -97,13 +97,19 @@ function wc_add_subscription_bs($user_email, $product_id)
 	$period   = WC_Subscriptions_Product::get_period($product_id);
 	$interval = WC_Subscriptions_Product::get_interval($product_id);
 
+	$date['start_date']   = date('Y-m-d H:i:s', strtotime('2017/6/1'));
+	$date['next_payment'] = date('Y-m-d H:i:s', strtotime('2017/9/1'));
+
 	$sub_args = array(
+		'status'           => 'active',
 		'customer_id'      => $user->id,
+		'start_date'       => $date['start_date'],
 		'billing_period'   => $period,
 		'billing_interval' => $interval,
 	);
 
 	$subscription = wcs_create_subscription($sub_args);
+	$subscription->update_dates(array('schedule_next_payment' => $date['next_payment']));
 	$subscription->add_product(wc_get_product($product_id), $quantity, $price_args);
 	$subscription->calculate_totals();
 }
