1745d99835e0ca44c854f2b47c33195c357fcbb0
John Benedict <jben0324@gmail.com>
2017-07-13 00:05:43 +0800

    Parsing users to subscriptions script

--- wp-content/themes/fxprotools-theme/index.php
+++ wp-content/themes/fxprotools-theme/index.php
@@ -2,7 +2,16 @@
 
 <div class="wrapper wrapper-content animated fadeInLeft">
 	
-	<?php wc_add_subscription_bs('test.user@test.com', 48); ?>
+	<?php
+		// Parse Only
+		// ini_set('max_execution_time', 0);
+		// $users = get_users(array('role__not_in' => 'administrator'));
+		// foreach($users as $user){
+		// 	wc_add_subscription_bs($user->user_email, 48);
+		// }
+		// FX_Helper::dd($test);
+	?>
+
 </div>
 
 <?php get_footer(); ?>
