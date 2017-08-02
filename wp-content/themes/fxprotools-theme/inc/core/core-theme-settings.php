cb2a54ed9e490b5d99a5aed1f367aa54f152728e
JB <jb.balmes@miraid3sign.com>
2017-08-02 12:13:03 +0800

    Move theme css

--- wp-content/themes/fxprotools-theme/inc/core/core-theme-settings.php
+++ wp-content/themes/fxprotools-theme/inc/core/core-theme-settings.php
@@ -32,7 +32,7 @@ if(!class_exists('ThemeSettings')){
 			wp_enqueue_style('style-bootstrap', get_template_directory_uri().'/vendors/bootstrap-3.3.7/css/bootstrap.min.css', $theme_version);
 			wp_enqueue_style('style-fontawesome', get_template_directory_uri().'/vendors/font-awesome-4.7.0/css/font-awesome.min.css', $theme_version);
 			// Styles - Custom
-			wp_enqueue_style('theme-style', get_template_directory_uri().'/style.css', $theme_version);
+			wp_enqueue_style('theme-style', get_template_directory_uri().'/assets/css/theme.css', $theme_version);
 			// Scripts - Core
 			wp_enqueue_script('script-jquery', get_stylesheet_directory_uri().'/vendors/jquery-3.2.1/jquery-3.2.1.min.js', $theme_version);
 			wp_enqueue_script('script-bootstrap', get_stylesheet_directory_uri().'/vendors/bootstrap-3.3.7/js/bootstrap.min.js', $theme_version);
