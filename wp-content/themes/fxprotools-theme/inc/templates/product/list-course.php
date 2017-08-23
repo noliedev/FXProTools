f1e24804d94e32b04efdbdd01271582ada8955e9
ben <benedicksahagun@icloud.com>
2017-08-23 16:47:57 +0800

    added dynamic progress bar

--- wp-content/themes/fxprotools-theme/inc/templates/product/list-course.php
+++ wp-content/themes/fxprotools-theme/inc/templates/product/list-course.php
@@ -18,11 +18,7 @@
 				<a href="<?php the_permalink(); ?>" class="btn btn-default block">Learn More</a>
 			</div>
 			<div class="col-md-12">
-				<div class="progress">
-				 	<div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: 25%">
-						25%
-				 	</div>
-				</div>
+				<?php get_template_part('inc/templates/product/progressbar'); ?>
 			</div>
 		</div>
 	</div>
---
+++ wp-content/themes/fxprotools-theme/inc/templates/product/progressbar.php
@@ -0,0 +1,11 @@
+<?php
+
+$progress = learndash_course_progress(array( 'course_id' => get_the_ID(), 'array' => true) ); 
+
+?>
+
+<div class="progress">
+ 	<div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: <?php echo $progress['percentage'];?>%">
+		<?php echo $progress['percentage'];?>%
+ 	</div>
+</div>
