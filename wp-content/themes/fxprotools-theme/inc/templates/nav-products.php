624d215eca540b121d848ebd8e46eda9b128f966
ben <benedicksahagun@icloud.com>
2017-08-23 14:58:38 +0800

    modified training pages

--- wp-content/themes/fxprotools-theme/inc/templates/nav-products.php
+++ wp-content/themes/fxprotools-theme/inc/templates/nav-products.php
@@ -3,9 +3,9 @@
 		<div class="row">
 			<div class="col-md-12">
 				<ul class="fx-nav-options">
-					<li class="dashboard"><a href="<?php bloginfo('url');?>/product/basic" title="Product Icon"><i class="fa fa-th-large" aria-hidden="true"></i></a></li>
-					<li><a href="<?php bloginfo('url');?>/product/basic">Basic Training</a></li>
-					<li><a href="<?php bloginfo('url');?>/product/advanced-course">Advance Training</a></li>
+					<li class="dashboard"><a href="<?php bloginfo('url');?>/product/basic-training" title="Product Icon"><i class="fa fa-th-large" aria-hidden="true"></i></a></li>
+					<li><a href="<?php bloginfo('url');?>/product/basic-traini">Basic Training</a></li>
+					<li><a href="<?php bloginfo('url');?>/product/advanced-training">Advance Training</a></li>
 					<li><a href="<?php bloginfo('url');?>/product/scanner">Market Scanner</a></li>
 					<li><a href="<?php bloginfo('url');?>/product/autotrader">Auto Trader</a></li>
 					<li><a href="<?php bloginfo('url');?>/product/sessions">Coaching / Webinars </a></li>
