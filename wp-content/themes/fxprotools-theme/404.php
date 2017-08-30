cf106a60a45d584cd951a6437afd3abd7052b9ac
John Benedict <jben0324@gmail.com>
2017-08-30 20:31:52 +0800

    Added 404 file

---
+++ wp-content/themes/fxprotools-theme/404.php
@@ -0,0 +1,12 @@
+<?php get_header(); ?>
+
+<!-- Temporary -->
+<div class="container">
+	<div class="row">
+		<div class="col-md-12">
+			<p>Page Not Found</p>
+		</div>
+	</div>
+</div>
+
+<?php get_footer(); ?>
