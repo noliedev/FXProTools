8a0ae78a6fcb7874fdb2a0a326d3b0e42cb4142d
John Benedict <jben0324@gmail.com>
2017-09-02 00:31:10 +0800

    Landing Page(Partial)

--- wp-content/themes/fxprotools-theme/index.php
+++ wp-content/themes/fxprotools-theme/index.php
@@ -1,7 +1,65 @@
 <?php get_header(); ?>
-	<div class="container">
-		<?php while ( have_posts() ) : the_post(); ?>
-			<?php the_content(); ?>
-		<?php endwhile; ?>
+	
+	<div class="fx-landing">
+		<div class="fx-heading">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-6">
+						<a href="<?php bloginfo('url'); ?>" class="logo">Fx Pro Tools</a>
+					</div>
+					<div class="col-md-6">
+						<ul class="fx-nav">
+							<li><a href="<?php bloginfo('url'); ?>/login">Members Login</a></li>
+							<li><a href="#">Support</a></li>
+						</ul>
+					</div>		
+				</div>
+			</div>
+		</div>
+		<div class="fx-red-note">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+						<p>CopyProfitShare is the map that teaches you specialized market knowledge!</p>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="fx-splash">
+			<div class="image-background">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-12">
+							<p class="intro-note"><span>Start</span> trading forex</p>
+							<p class="intro-note">like a pro <span>right now!</span></p>
+							<div class="video"></div>
+							<a href="#" class="btn btn-danger btn-lg">
+								Create your Account Now!
+								<span>Sign up takes less than 60 seconds</span>
+							</a>
+						</div>
+						<div class="col-md-4">
+							<div class="item-info">
+								<i class="fa fa-circle icon"></i>
+								<p>Gain full Access to <br/> CopyProfitShare's training <br/> videos &amp; courses.</p>
+							</div>
+						</div>
+						<div class="col-md-4">
+							<div class="item-info">
+								<i class="fa fa-circle icon"></i>
+								<p>Full Acess to CPS Forex &amp; <br/> Binary Rooms Live <br/> Signals 24 hours.</p>
+							</div>
+						</div>
+						<div class="col-md-4">
+							<div class="item-info">
+								<i class="fa fa-circle icon"></i>
+								<p>Get access to trading <br/> webinars where you can <br/> copy trades live.</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
 	</div>
+
 <?php get_footer(); ?>
