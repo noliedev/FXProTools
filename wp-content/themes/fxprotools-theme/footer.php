42b2fce0bd688444fb72f88d0b2435cf5cdb4183
JB <jb.balmes@miraid3sign.com>
2017-09-05 16:37:45 +0800

    Added Footer - including condition when displaying

--- wp-content/themes/fxprotools-theme/footer.php
+++ wp-content/themes/fxprotools-theme/footer.php
@@ -1,3 +1,19 @@
+	<?php if(!is_page(array('login', 'forgot-password')) ): ?>
+	<div class="fx-footer">
+		<div class="container">
+			<div class="col-md-12">
+				<p class="text-center">Trading involves significant risk.</p>
+				<ul class="fx-nav-footer">
+					<li><a href="#">General Disclaimer</a></li>
+					<li><a href="#">Risk Disclaimer</a></li>
+					<li><a href="#">Privacy Policy</a></li>
+					<li><a href="#">Trading Signals Disclaimer</a></li>
+				</ul>
+			</div>
+		</div>
+	</div>
+	<?php endif; ?>
+
 </div><!-- /fx-wrapper -->
 <?php wp_footer(); ?>
 
