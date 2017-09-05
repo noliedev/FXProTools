5cc6643b1e33803dd43361e4c1352ae506cbc5b1
JB <jb.balmes@miraid3sign.com>
2017-09-05 16:37:05 +0800

    Landing Page Update

--- wp-content/themes/fxprotools-theme/index.php
+++ wp-content/themes/fxprotools-theme/index.php
@@ -33,7 +33,7 @@
 							<p class="intro-note"><span>Start</span> trading forex</p>
 							<p class="intro-note">like a pro <span>right now!</span></p>
 							<div class="video"></div>
-							<a href="#" class="btn btn-danger btn-lg">
+							<a href="#" class="btn btn-danger btn-lg btn-create-account">
 								Create your Account Now!
 								<span>Sign up takes less than 60 seconds</span>
 							</a>
@@ -64,11 +64,11 @@
 			<div class="container">
 				<div class="row">
 					<div class="col-md-12">
-						<p class="note text-center">Find out why we are the <br/> <span>#1 choice for traders</span> worldwide!</p>
+						<p class="note text-center">Find out why we are the <br/> <span class="text-bold">#1 choice for traders</span> worldwide!</p>
 					</div>
 					<div class="clearfix"></div>
 					<div class="col-md-6">
-						<img src="http://via.placeholder.com/450x280" class="img-responsive centered-block">
+						<img src="<?php bloginfo('template_url'); ?>/assets/img/macbook.png" class="img-responsive centered-block">
 					</div>
 					<div class="col-md-6">
 						<ul class="list-features fa-ul">
@@ -119,7 +119,7 @@
 				</div>
 			</div>
 		</div>
-		<!-- <div class="section-four">
+		<div class="section-four">
 			<div class="container">
 				<div class="row">
 					<div class="col-md-12">
@@ -127,16 +127,90 @@
 							<div class="panel-body">
 								<div class="row">
 									<div class="col-md-12">
-										<div class="heading">
+										<div class="heading m-b-lg">
 											<h1>What is Forex &amp; Binary Trading..</h1>
 											<span>Why Should You Learn It?</span>
 										</div>
 									</div>
 									<div class="col-md-6">
-										
+										<p class="note m-b-md"><span class="text-bold">If this is your first time here,</span><br/> then you're probably wondering,</p>
+										<p class="note m-b-md">"WHAT IS FOREX or BINARY TRADING?"</p>
+										<p class="note m-b-md">and</p>
+										<p class="note m-b-md">"HOW DOES LEARNING THIS BENEFIT ME?" </p>
+										<p class="note">Am I right?</p>
 									</div>
 									<div class="col-md-6">
-
+										<p class="note text-center text-bold">What is Forex &amp; Binary Trading?</p>
+										<img src="http://via.placeholder.com/575x322?text=Video" class="img-responsive centered-block m-b-sm">
+										<p class="note text-center text-bold">What This Short Video</p>
+									</div>
+									<div class="col-md-12">
+										<div class="lg-note m-t-lg">
+											<span>Let Me Show You Why Tens Of Thousands Of Individuals</span>
+											<span class="green">Just Like You</span>
+										</div>
+										<div class="md-note text-center">
+											<p class="text-bold">Are Currently Learning Forex & Binary Trading From CopyProfitShare</p>
+											<p>...But first before we do that. Here is a quick history of Forex and Binary Trading to help you understand what it is, and more importantly, why do YOU want to learn it...</p>
+										</div>
+										<div class="panel panel-steps m-t-lg m-b-lg">
+											<div class="panel-body">
+												<ul class="list-steps">
+													<li>
+														<div class="counter">
+															<span class="number">1.</span>
+															<span class="bar one"></span>
+															<span class="bar two"></span>
+														</div>
+														<h2><span class="bg-red-text">Did You Know That?</span> Forex & Binary Trading Has Been Around Since The 1970s</h2>
+														<p>...But You Had To Have  A LOT Of Money. You Also Had To Use A Landline To Place Your “Orders” Over The Phone...</p>
+														<div class="clearfix"></div>
+													</li>
+													<li>
+														<div class="counter">
+															<span class="number">2.</span>
+															<span class="bar one"></span>
+															<span class="bar two"></span>
+														</div>
+														<h2>Online Trading Was Introduced In The 1990s</h2>
+														<p>...But You Had to Have A LOT of Money. You We’re Only Allowed To Use One or Two Platforms Trading Platforms...</p>
+														<div class="clearfix"></div>
+													</li>
+													<li>
+														<div class="counter">
+															<span class="number">3.</span>
+															<span class="bar one"></span>
+															<span class="bar two"></span>
+														</div>
+														<h2>Present Day Technolgy & Software Increases Market Participation & Availability</h2>
+														<p>...Multiple Brokers Opened Up, Mobile Trading Is Introduced & More Currency Open Up To Be Traded Globally... </p>
+														<div class="clearfix"></div>
+													</li>
+													<li>
+														<div class="counter">
+															<span class="number">4.</span>
+															<span class="bar one"></span>
+															<span class="bar two"></span>
+														</div>
+														<h2>Invidviduals Can Now Getting “A Piece” Of The <span class="bg-red-text">$4 Trillion Dollars</span> In Daily Currency Turnover</h2>
+														<p>...Every Day People Can Now “Get A Piece Of The Action”  With The Right “Knowledge” and Trading Platforms...</p>
+														<div class="clearfix"></div>
+													</li>
+													<li>
+														<div class="counter">
+															<img src="<?php bloginfo('template_url'); ?>/assets/img/burglar.png" class="img-responsive">
+														</div>
+														<h2>Invidviduals Can Now Getting “A Piece” Of The <span class="red">$4 Trillion Dollars</span> In Daily Currency Turnover</h2>
+														<p>...Every Day People Can Now “Get A Piece Of The Action”  With The Right “Knowledge” and Trading Platforms...</p>
+														<div class="clearfix"></div>
+													</li>
+												</ul>
+											</div>
+										</div>
+										<h2 class="text-center text-bold text-heading">Get &amp; Stay Ahead In The Markets!</h2>
+										<p class="text-center text-bold text-heading-md bg-red-text m-b-md">Our Methods Pratically Allow You To “Legally Steal” From That $4 Trillon Dollars In Currency Turnover...</p>
+										<p class="text-heading-sm">...If you've tried in the past to piece together dozens of other trading strategies, but they failed to produce consistent positive results...</p>
+										<p class="text-heading-sm">Then I want to to watch this quick video from one of our Top CopyProfitShare Members that will show you how simple your life can become when you start using our platform:</p>
 									</div>
 								</div>
 							</div>
@@ -144,7 +218,35 @@
 					</div>
 				</div>
 			</div>
-		</div> -->
+		</div>
+		<div class="section-five">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+						<div class="image-market-trading">
+							<div class="row">
+								<div class="col-md-10 col-md-offset-1">
+									<div class="content">
+										<h2 class="text-bold text-center text-heading">
+											<span class="bg-red-text">Cut The Amount Of Time, Stress & Anxiety</span><br/> and Suffering Behind A Keyboard So You Can Deal With What Actually Matters... 
+										</h2>
+										<div class="m-t-lg m-b-lg">
+											<img src="http://via.placeholder.com/960x500?text=Video" class="img-responsive centered-block m-b-sm">
+										</div>
+										<div class="text-center">
+											<a href="#" class="btn btn-danger btn-lg btn-create-account">
+												Create Your Account Today!
+												<span>Sign up takes less than 60 seconds</span>
+											</a>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
 	</div>
 
 <?php get_footer(); ?>
