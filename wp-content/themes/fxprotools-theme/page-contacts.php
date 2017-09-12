882dd8a44aeab7c667a52c3556e65fbd97dabae3
elvin <jonelvinnaorbe@gmail.com>
2017-09-12 17:48:48 +0800

    Update contacts page

--- wp-content/themes/fxprotools-theme/page-contacts.php
+++ wp-content/themes/fxprotools-theme/page-contacts.php
@@ -15,108 +15,105 @@
 				<div class="fx-search-contact">
 					<div class="panel panel-default">
 						<div class="panel-body">
-							<div class="input-group">
-								<div class="input-group-addon"><i class="fa fa-search"></i></div>
-								<input type="text" class="form-control" placeholder="Search by name or e-mail">
-							</div>
+							<form action="" method="GET">
+								<div class="input-group">
+									<div class="input-group-addon"><i class="fa fa-search"></i></div>
+									<input type="text" class="form-control" name="search" placeholder="Search by name or e-mail">
+								</div>
+							</form>
 						</div>
 					</div>
 				</div>
 				<div class="clearfix"></div>
+
 				<ul class="fx-list-contacts">
-					<li>
-						<div class="media">
-							<div class="media-left">
-								<img src="http://via.placeholder.com/55x55?text=Image" class="media-object">
-							</div>
-							<div class="media-body">
-								<div class="info">
-									<h5 class="media-heading text-bold">Full Name</h5>
-									<p>user-email@gmail.com</p>
+					<?php 
+						$referrals = get_user_referrals();
+						$ref_count = 0;
+						$contacts = array();
+						$results = array();
+						$search_results = array();
+
+						foreach($referrals as $referral){
+							$order = wc_get_order( $referral->reference );
+							$contacts[$ref_count]['id'] = $order->get_user_id();
+							$contacts[$ref_count]['username'] = get_the_author_meta('user_login', $order->get_user_id());
+							$contacts[$ref_count]['fname'] = get_the_author_meta('first_name', $order->get_user_id());
+							$contacts[$ref_count]['lname'] = get_the_author_meta('last_name', $order->get_user_id());
+							$contacts[$ref_count]['email'] = get_the_author_meta('email', $order->get_user_id());
+							$contacts[$ref_count]['date'] = time_elapsed_string($order->order_date);
+							$contacts[$ref_count]['avatar'] = get_avatar($order->get_user_id());
+							$ref_count++;
+						}
+
+						if($_GET['search']){
+							foreach ($contacts as $index => $index_item) {
+						       	foreach($index_item as $item){
+						       		if(stripos($item,$_GET['search']) !== false){
+						       			if(!in_array($index, $results,TRUE)){
+						       				array_push($results, $index);
+						       			}
+						       		}
+						       	}
+						    }
+						    foreach($results as $result){
+						    	array_push($search_results,$contacts[$result]);
+						    }
+						    $contacts = $search_results;
+						}
+
+						$per_page = 1;
+						$total_rows = count($contacts);
+						$pages = ceil($total_rows / $per_page);
+						$current_page = isset($_GET['i']) ? $_GET['i'] : 1;
+						$current_page = ($total_rows > 0) ? min($pages, $current_page) : 1;
+						$start = $current_page * $per_page - $per_page;
+						$slice = array_slice($contacts, $start, $per_page);
+
+						foreach($slice as $k => $v){
+					?>
+
+						<li>
+							<div class="media">
+								<div class="media-left">
+									<?php echo $v['avatar']; ?>
 								</div>
-								<div class="actions">
-									<span class="small">2 days ago</span>
-									<a href="<?php bloginfo('url');?>/marketing/contacts/user" class="btn btn-default btn-sm m-l-sm">View</a>
-								</div>
-							</div>
-						</div>
-					</li>
-					<li>
-						<div class="media">
-							<div class="media-left">
-								<img src="http://via.placeholder.com/55x55?text=Image" class="media-object">
-							</div>
-							<div class="media-body">
-								<div class="info">
-									<h5 class="media-heading text-bold">Full Name</h5>
-									<p>user-email@gmail.com</p>
-								</div>
-								<div class="actions">
-									<span class="small">2 days ago</span>
-									<a href="<?php bloginfo('url');?>/marketing/contacts/user" class="btn btn-default btn-sm m-l-sm">View</a>
-								</div>
-							</div>
-						</div>
-					</li>
-					<li>
-						<div class="media">
-							<div class="media-left">
-								<img src="http://via.placeholder.com/55x55?text=Image" class="media-object">
-							</div>
-							<div class="media-body">
-								<div class="info">
-									<h5 class="media-heading text-bold">Full Name</h5>
-									<p>user-email@gmail.com</p>
-								</div>
-								<div class="actions">
-									<span class="small">2 days ago</span>
-									<a href="<?php bloginfo('url');?>/marketing/contacts/user" class="btn btn-default btn-sm m-l-sm">View</a>
-								</div>
-							</div>
-						</div>
-					</li>
-					<li>
-						<div class="media">
-							<div class="media-left">
-								<img src="http://via.placeholder.com/55x55?text=Image" class="media-object">
-							</div>
-							<div class="media-body">
-								<div class="info">
-									<h5 class="media-heading text-bold">Full Name</h5>
-									<p>user-email@gmail.com</p>
-								</div>
-								<div class="actions">
-									<span class="small">2 days ago</span>
-									<a href="<?php bloginfo('url');?>/marketing/contacts/user" class="btn btn-default btn-sm m-l-sm">View</a>
-								</div>
-							</div>
-						</div>
-					</li>
-					<li>
-						<div class="media">
-							<div class="media-left">
-								<img src="http://via.placeholder.com/55x55?text=Image" class="media-object">
-							</div>
-							<div class="media-body">
-								<div class="info">
-									<h5 class="media-heading text-bold">Full Name</h5>
-									<p>user-email@gmail.com</p>
-								</div>
-								<div class="actions">
-									<span class="small">2 days ago</span>
-									<a href="<?php bloginfo('url');?>/marketing/contacts/user" class="btn btn-default btn-sm m-l-sm">View</a>
+								<div class="media-body">
+									<div class="info">
+										<h5 class="media-heading text-bold">
+											<?php  
+												if($v['fname']){
+													echo $v['fname'] . ' ' . $v['lname'];
+												}else{
+													echo $v['username'];
+												}
+											?>
+										</h5>
+										<p><?php echo $v['email']; ?></p>
+									</div>
+									<div class="actions">
+										<span class="small"><?php echo $v['date']; ?></span>
+										<a href="<?php bloginfo('url');?>/marketing/contacts/user" class="btn btn-default btn-sm m-l-sm">View</a>
+									</div>
 								</div>
 							</div>
-						</div>
-					</li>
+						</li>
 				</ul>
 				<div class="text-center">
 					<ul class="pagination">
-						<li><a href="#">1</a></li>
-						<li><a href="#">2</a></li>
-						<li><a href="#">3</a></li>
-						<li><a href="#">4</a></li>
-						<li><a href="#">5</a></li>
+						<?php
+							}
+
+							if ($pages > 0) {
+							    $counter = 1;
+							    while($counter <= $pages){
+						?>
+							    	<li class="<?php if($current_page == $counter){ echo "active"; } ?>"><a href="<?php echo get_the_permalink(); ?>?i=<?php echo $counter; ?><?php if($_GET['search']){ echo "&search=" . $_GET['search']; } ?>"><?php echo $counter ?></a></li>
+						<?php
+									$counter++;
+							    }
+							}
+						?>
 					</ul>
 				</div>
 			</div>
