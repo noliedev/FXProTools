5692b8364d84bb053f794cb826f3e93ecc9ec10c
John Benedict <jben0324@gmail.com>
2017-09-17 02:02:48 +0800

    Signals Page - Partial

--- wp-content/themes/fxprotools-theme/index.php
+++ wp-content/themes/fxprotools-theme/index.php
@@ -1,23 +1,7 @@
 <?php get_header(); ?>
 	
-	<div class="fx-landing">
-		<?php if( !is_user_logged_in() ): ?>
-		<div class="section-one">
-			<div class="container">
-				<div class="row">
-					<div class="col-md-6">
-						<a href="<?php bloginfo('url'); ?>" class="logo">Fx Pro Tools</a>
-					</div>
-					<div class="col-md-6">
-						<ul class="fx-nav">
-							<li><a href="<?php bloginfo('url'); ?>/login">Members Login</a></li>
-							<li><a href="#">Support</a></li>
-						</ul>
-					</div>		
-				</div>
-			</div>
-		</div>
-		<?php endif; ?>
+	<div class="fx-landing main">
+		<?php get_template_part('inc/nav-capture-page'); ?>
 		<div class="fx-red-note">
 			<div class="container">
 				<div class="row">
---
+++ wp-content/themes/fxprotools-theme/page-signals.php
@@ -0,0 +1,56 @@
+<?php get_header(); ?>
+	
+<div class="fx-landing signals">
+	<?php get_template_part('inc/nav-capture-page'); ?>
+	<div class="fx-red-note">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<p>CopyProfitShare is the map that teaches you specialized market knowledge!</p>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="section-two">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-10 col-md-offset-1">
+					<div class="text-center">
+						<p class="intro-note m-b-xs">Improve Your Trades With</p>
+						<p class="intro-note lg"><strong>UNLIMITED Professional</strong></p>
+					</div>
+					<img src="<?php bloginfo('template_url'); ?>/assets/img/signals.png" class="img-responsive center-block m-t-md m-b-lg">
+					<div class="text-center">
+						<a href="#" class="btn btn-success btn-lg btn-lg-w-text">
+							Get Your Access For $1.00 Trial Now!
+							<span>Sign up takes less than 60 seconds. Pick a plan to get started</span>
+						</a>
+					</div>
+				</div>
+				<div class="clearfix"></div>
+			</div>
+
+		</div>
+	</div>
+	<div class="section-three section-w-panel">
+		<div class="container n-t-xl">
+			<div class="row">
+				<div class="col-md-12">
+					<div class="header-text text-center">
+						<img src="<?php bloginfo('template_url'); ?>/assets/img/arrow-left.png" class="img-responsive arrow-left">
+						<h2 class="m-t-none m-b-none inline-block">Our Previous Results!</h2>
+						<img src="<?php bloginfo('template_url'); ?>/assets/img/arrow-right.png" class="img-responsive arrow-right">
+					</div>
+					<div class="panel">
+						<div class="panel-body">
+							<img src="<?php bloginfo('template_url'); ?>/assets/img/signals-ss.jpg" class="img-responsive center-block">
+							<p class="signals-disclosure m-t-md">*Future Results May Vary for Different Users & Market Movements*</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<?php get_footer(); ?>
