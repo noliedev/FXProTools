e00824a6145ef16963d065d8732d0793da3ee3b5
John Benedict <jben0324@gmail.com>
2017-09-16 20:50:59 +0800

    Added Capture Page - Funnel 1

---
+++ wp-content/themes/fxprotools-theme/page-f1.php
@@ -0,0 +1,95 @@
+<?php get_header(); ?>
+
+<div class="fx-capture-page f1">
+	<div class="fx-red-note">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<p>The Map That Teaches You Specialized Market Knowledge! </p>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="section-one">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-5">
+					<div class="content-wrapper">
+						<div class="content">
+							<div class="text-center">
+								<span>An Exclusive Message from Industy Leaders...</span>
+								<h1>Join The Exciting Market Of  Foreign Exchange Trading</h1>
+								<p>Our elite group of traders have achieved master level profits with strategies & techniques learned directly from our training courses. Achieving profits of $4,954.35 & higher at night while they sleep!</p>
+							</div>
+							<div class="fx-input-email">
+								<input type="email" class="form-control" placeholder="E-mail ...">
+							</div>
+							<div class="text-center m-t-md">
+								<a href="#" class="btn btn-danger">Click To Get Access</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="section-two">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<div class="header-text">
+						<span>Sneak Peek Of What You Get With Us</span>
+						<h2>Everything You Need To Become A Better Market Trader</h2>
+						<span class="red"></span>
+					</div>
+				</div>
+			</div>
+			<div class="row row-item-info">
+				<div class="col-md-3">
+					<div class="item-info">
+						<div class="image">
+							<img src="<?php bloginfo('template_url'); ?>/assets/img/ico-dark1.png" class="centered-block img-responsive">
+						</div>
+						<span>Interactive <br/>Education Lessons</span>
+					</div>
+				</div>
+				<div class="col-md-3">
+					<div class="item-info">
+						<div class="image">
+							<img src="<?php bloginfo('template_url'); ?>/assets/img/ico-dark2.png" class="centered-block img-responsive">
+						</div>
+						<span>Live 24 Hour <br/>Signals</span>
+					</div>
+				</div>
+				<div class="col-md-3">
+					<div class="item-info">
+						<div class="image">
+							<img src="<?php bloginfo('template_url'); ?>/assets/img/ico-dark3.png" class="centered-block img-responsive">
+						</div>
+						<span>Live Trading <br/>Webinars</span>
+					</div>
+				</div>
+				<div class="col-md-3">
+					<div class="item-info">
+						<div class="image">
+							<img src="<?php bloginfo('template_url'); ?>/assets/img/ico-dark4.png" class="centered-block img-responsive">
+						</div>
+						<span>Updates On <br/>Market News</span>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="fx-red-banner">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-8 col-md-offset-2">
+					<h2 class="m-t-none"2>Confidently Trade the Market with Our Expert Help</h2>
+					<a href="#" class="btn btn-lg btn-outline m-t-xs">Click To Get Access</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<?php get_footer(); ?>
