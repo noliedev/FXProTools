6b966f6830d70cc7b15d90d2ca82aef755d85647
John Benedict <jben0324@gmail.com>
2017-09-28 22:57:59 +0800

    Revert order of displaying funnels

--- wp-content/themes/fxprotools-theme/inc/function-custom.php
+++ wp-content/themes/fxprotools-theme/inc/function-custom.php
@@ -116,7 +116,7 @@ function get_funnels()
 	$args = array(
 		'posts_per_page'   => -1,
 		'orderby'          => 'menu_order',
-		'order'			   => 'DESC',
+		'order'			   => 'ASC',
 		'post_status'      => 'publish',
 		'post_type'		   => 'fx_funnel',
 	);
