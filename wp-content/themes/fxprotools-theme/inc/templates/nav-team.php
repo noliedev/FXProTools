1325e1f52509653dde8fcd2db7c5c730e1c12b13
John Benedict <jben0324@gmail.com>
2017-09-30 22:12:57 +0800

    Updated Sub Navigation for Team Page

--- wp-content/themes/fxprotools-theme/inc/templates/nav-team.php
+++ wp-content/themes/fxprotools-theme/inc/templates/nav-team.php
@@ -4,11 +4,11 @@
 			<div class="col-md-12">
 				<ul class="fx-nav-options">
 					<li class="dashboard"><a href="#" title="Team Icon"><i class="fa fa-th-large" aria-hidden="true"></i></a></li>
-					<li><a href="#">Referred Members</a></li>
-					<li><a href="#">Direct Downline</a></li>
-					<li><a href="#">Unilevel Tree View</a></li>
-					<li><a href="#">Genaology Tree</a></li>
-					<li><a href="#">Direct Upline</a></li>
+					<li><a href="<?php bloginfo('url'); ?>/referred-members/">Referred Members</a></li>
+					<li><a href="<?php bloginfo('url'); ?>/direct-downline/">Direct Downline</a></li>
+					<li><a href="<?php bloginfo('url'); ?>/unilevel-tree-view">Unilevel Tree View</a></li>
+					<li><a href="<?php bloginfo('url'); ?>/genealogy-tree">Genaology Tree</a></li>
+					<li><a href="<?php bloginfo('url'); ?>/direct-upline">Direct Upline</a></li>
 				</ul>
 			</div>
 		</div>
